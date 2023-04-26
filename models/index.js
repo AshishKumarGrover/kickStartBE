839b68c3eb1c60daeff8f42707a7a2ed737f263c
Vishal Vashisht <vishal.vashisht@investwellonline.com>
2023-04-26 11:20:48 +0530

    minor changes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,3 @@
-const {scheme} = require('./schemeModel.js')
-
-
 const Sequelize = require('sequelize')
 const config = require(__dirname + '/../config/db.json').mysql
 let sequelize = null
@@ -14,94 +11,86 @@ sequelize.authenticate().then(() => {
 
 
 
-// const scheme = sequelize.define('schemes', {
-//   fundid: {
-//     type: Sequelize.DataTypes.INTEGER,
-//     allowNull: false
-//   },
-//   schid: {
-//     type: Sequelize.DataTypes.INTEGER,
-//     allowNull: false,
-//     primaryKey: false
-//   },
-//   name: {
-//     type: Sequelize.DataTypes.STRING(150),
-//     allowNull: true
-//   },
-//   iwellCode: {
-//     type: Sequelize.DataTypes.STRING(20),
-//     allowNull: true,
-//     unique: true
-//   },
-//   camsCode1: {
-//     type: Sequelize.DataTypes.STRING(15),
-//     allowNull: true
-//   },
-//   camsCode2: {
-//     type: Sequelize.DataTypes.STRING(15),
-//     allowNull: true
-//   },
-//   fsid: {
-//     type: Sequelize.DataTypes.STRING(50),
-//     allowNull: true
-//   },
-//   objectiveid: {
-//     type: Sequelize.DataTypes.INTEGER(5),
-//     allowNull: true
-//   },
-//   productType: {
-//     type: Sequelize.DataTypes.CHAR(2),
-//     allowNull: true
-//   }
-// })
+const scheme = sequelize.define('schemes', {
+  fundid: {
+    type: Sequelize.DataTypes.INTEGER,
+    allowNull: false
+  },
+  schid: {
+    type: Sequelize.DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: false
+  },
+  name: {
+    type: Sequelize.DataTypes.STRING(150),
+    allowNull: true
+  },
+  iwellCode: {
+    type: Sequelize.DataTypes.STRING(20),
+    allowNull: true,
+    unique: true
+  },
+  camsCode1: {
+    type: Sequelize.DataTypes.STRING(15),
+    allowNull: true
+  },
+  camsCode2: {
+    type: Sequelize.DataTypes.STRING(15),
+    allowNull: true
+  },
+  fsid: {
+    type: Sequelize.DataTypes.STRING(50),
+    allowNull: true
+  },
+  objectiveid: {
+    type: Sequelize.DataTypes.INTEGER(5),
+    allowNull: true
+  },
+  productType: {
+    type: Sequelize.DataTypes.CHAR(2),
+    allowNull: true
+  }
+})
 
-// const navHistory = sequelize.define('navHistory', {
-//   schid: {
-//     type: Sequelize.DataTypes.INTEGER,
-//     allowNull: true
-//   },
-//   navDate: {
-//     type: Sequelize.DataTypes.DATEONLY,
-//     allowNull: true
-//   },
-//   nav: {
-//     type: Sequelize.DataTypes.FLOAT(18, 4),
-//     allowNull: true
-//   },
-//   iwellCode: {
-//     type: Sequelize.DataTypes.STRING(20),
-//     allowNull: false
-//   },
-//   age: {
-//     type: Sequelize.DataTypes.CHAR(1),
-//     allowNull: true
-//   },
-//   isNew: {
-//     type: Sequelize.DataTypes.INTEGER(1),
-//     allowNull: true,
-//     defaultValue: '0'
-//   },
-//   manual: {
-//     type: Sequelize.DataTypes.INTEGER(1),
-//     allowNull: true
-//   }
-// })
-
-const navHistory = 'lol'
-
+const navHistory = sequelize.define('navHistory', {
+  schid: {
+    type: Sequelize.DataTypes.INTEGER,
+    allowNull: true
+  },
+  navDate: {
+    type: Sequelize.DataTypes.DATEONLY,
+    allowNull: true
+  },
+  nav: {
+    type: Sequelize.DataTypes.FLOAT(18, 4),
+    allowNull: true
+  },
+  iwellCode: {
+    type: Sequelize.DataTypes.STRING(20),
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.DataTypes.CHAR(1),
+    allowNull: true
+  },
+  isNew: {
+    type: Sequelize.DataTypes.INTEGER(1),
+    allowNull: true,
+    defaultValue: '0'
+  },
+  manual: {
+    type: Sequelize.DataTypes.INTEGER(1),
+    allowNull: true
+  }
+})
 
 sequelize.sync().then(() => {
 }).catch((error) => {
   console.error("unable create table", error)
 })
 
-console.log('ppppppppppppppppppppppppppp',sequelize)
-
 scheme.Sequelize = Sequelize
 navHistory.Sequelize = Sequelize
-
-// module.exports = sequelize;
-// console.log(sequelize);
 
 module.exports = {
   scheme,
